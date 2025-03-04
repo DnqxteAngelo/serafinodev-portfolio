158c76112e56c30a78a622871e400fac5689782c
Angelo Serafino <serafino1412@gmail.com>
2025-03-04 15:53:17 +0800

    Add favicon link to the head of the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" /></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
